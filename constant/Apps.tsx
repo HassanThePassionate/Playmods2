29cf9a337c3e5505f682d9cd099ab6c6cecb5a48
alihassan8989 <maqsoodia777@gmail.com>
2024-06-24 20:11:57 +0500

    Update

---
+++ constant/Apps.tsx
@@ -0,0 +1,74 @@
+const Apps = [
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/f2c84cd0-1634-4c2f-ac3e-20ca1eb5c5ce.png",
+    title: "Art & Design",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/f655334f-defc-414d-8462-a6f87a15098c.png",
+    title: "Emulator",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/6087facf-228f-45c6-9b27-3ded30bc78d6.png",
+    title: "Auto & Vehicles",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/62cf5d72-91a3-4343-b2d8-11ee08bb48b3.png",
+    title: "Beauty",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/5036f05b-7c28-4a03-8720-cedc4d99684a.png",
+    title: "Books",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/b311ac62-b8d9-4c13-95c8-d5d985b6ef30.png",
+    title: "Bussiness",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/3670748e-0e8c-431c-b56b-ce58a3e46efe.png",
+    title: "Comics",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/8facf2f0-ca5a-4f0b-ba2f-45ce7b47e4db.png",
+    title: "Communication",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/5e9f4b27-4952-4142-bbf0-c9357b0fc850.png",
+    title: "Education",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/a83eccbf-622c-4cb9-a330-d21d00decd35.png",
+    title: "Entertainment",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/d5adca59-8d19-4ac5-a3ba-e59446fa9cc1.png",
+    title: "Events",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/aaba429b-be2f-4b42-902d-e97408f566d0.png",
+    title: "Food & Drinks",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/57126394-1712-4560-96bb-b6ba2cd27d0c.png",
+    title: "Sport",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/46e6200a-49f5-41ee-b90a-8fef8170a55f.png",
+    title: "Health & Fitness",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/d6117283-aa31-48fe-b08a-339257250db1.png",
+    title: "House & Home",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/f5409601-6b34-4392-81dd-909b6066a985.png",
+    title: "Shopping",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/f8ed3bc5-308f-4510-a40a-a3268f2a377b.png",
+    title: "Social",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/4a561364-bcbc-498f-9785-ec81333d895a.png",
+    title: "Tools",
+  },
+];
---
+++ constant/Games.tsx
@@ -0,0 +1,75 @@
+const Games = [
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/ce7c43fa-eedb-41e9-9f27-e31e0f94b2db.png",
+    title: "Adventure",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/fee28a60-735e-4589-887b-b464ed0ad537.png",
+    title: "Arcade",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/9aea35db-385f-4bf4-8bdf-adef8ac1b940.png",
+    title: "Action",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/f45f9b16-2394-49b2-a412-221aa3a55545.png",
+    title: "Board",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/bdc1f5f7-0f54-4130-8f7d-ff028cec5299.png",
+    title: "Casual",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/8bcdaca6-b742-4c0c-b888-c2dbde21b0e9.png",
+    title: "Card",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/d75298d9-9f9f-498f-9024-bb902695bf7a.png",
+    title: "Education",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/5dfa4aee-9d5d-4591-9b7f-75d15f9edde0.png",
+    title: "Music",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/a19c0fa4-6b70-443a-8336-b125b5b8bf3e.png",
+    title: "Puzzle",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/e008ac58-9c55-4017-ad94-ed5566efec48.png",
+    title: "Racing",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/13404610-e6a1-44c0-a0e3-3b79122f0370.png",
+    title: "Role Playing",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/515a9c71-72d2-4173-a255-cafc39c714fb.png",
+    title: "Shooting",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/57126394-1712-4560-96bb-b6ba2cd27d0c.png",
+    title: "Sport",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/46dd00e7-65e3-4564-80e4-3b3ffbe8f9b4.png",
+    title: "Sandbox",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/8579711b-0b24-44c8-a887-7a6f39df155c.png",
+    title: "Simulation",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/0d47eb29-090a-4f5a-8692-916612c2b193.png",
+    title: "Strategy",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/9f8edd84-7354-4a2e-9cfc-891059233be4.png",
+    title: "Trivia",
+  },
+  {
+    img: "https://qn-resource.playmods.net/prd/105/20221009/4bde1303-e93e-4ef9-88ed-c147dcd2e851.png",
+    title: "Word",
+  },
+];
+export default Games;

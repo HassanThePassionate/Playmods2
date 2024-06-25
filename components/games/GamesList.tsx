1cee8f522f2bb96862b5fb717f64cbd7f18bb378
HassanThePassionate <maqsoodia777@gmail.com>
2024-06-25 17:23:27 +0500

    Update

--- components/games/GamesList.tsx
+++ components/games/GamesList.tsx
@@ -22,7 +22,7 @@ const GamesList = ({ data }: GameListProps) => {
         <Link
           href={`/game/${elem.id}`}
           key={elem.img}
-          className='flex gap-2 relative items-center link transition duration-300 rounded-md hover:bg-[#f3f3f3] hover:text-blue-600 px-[10px] py-[10px]'
+          className='flex gap-2 relative items-center link transition duration-300 rounded-md hover:bg-[#f3f3f3] hover:shadow-md hover:text-blue-600 px-[10px] py-[10px]'
         >
           <Image
             src={elem.img}
